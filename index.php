8821b2be95a2dcca5eddfd19410925c7feee1a01
NLTdev <NLT@NLTdev.com>
2019-12-01 18:08:52 +0100

    1

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="fr" amp>
+  <head>
+    <title>Alix & Nico</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Alix et Nico [...]. “Nous n'héritons pas de la terre de nos parents, nous l'empruntons à nos enfants.” #alixetnico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="873PgM1rLvDdWmxEgx7DjkJEL2PwZ0NCCaE6vZYc0h4">
+    <link rel="shortcut icon" href="https://img.alixetnico.com/&b.ico" type="image/x-icon">
+    <link rel="icon" href="https://img.alixetnico.com/&b.png" type="image/png">
+    <link rel="canonical" href="https://www.alixetnico.com/">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@alixetnico">
+    <meta property="og:title" content="Alix & Nico">
+    <meta property="og:image" content="https://img.alixetnico.com/card.png">
+    <meta property="og:type" content="Organization">
+    <meta property="og:url" content="https://www.alixetnico.com/">
+    <script type="application/ld+json">
+    {
+    "@context" : "http://schema.org",
+    "@type" : "Organization",
+    "name" : "Alix & Nico",
+    "url" : "https://www.alixetnico.com/",
+    "sameAs" :
+    [
+    "https://www.facebook.com/alixetnico/",
+    "https://twitter.com/alixetnico",
+    "https://www.instagram.com/alixetnico/",
+    "https://www.linkedin.com/company/alixetnico",
+    "https://medium.com/@alixetnico",
+    "https://www.youtube.com/channel/UClpBrbAmwShlRP17ywmWj8w"
+    ]
+    }
+    </script>
+    <style type="text/css" amp-custom>
+    body {padding:24px;text-align:center;font-family:courier;background-color:black;color:white;}
+    h1 {line-height:0px;}
+    h2 {color:black;line-height:24px;}
+    h2 a {color:#8f8f8f;text-decoration:none;}
+    p a {color:white;line-height:24px;}
+    </style>
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+  </head>  
+  <body>
+    <amp-analytics type="gtag" data-credentials="include">
+    <script type="application/json">
+    {
+    "vars" : {
+    "gtag_id": "<UA-152686922-1>",
+    "config" : {
+    "<UA-152686922-1>": { "groups": "default" }
+    }
+    }
+    }
+    </script>
+    </amp-analytics>
+       <h1>Alix & Nico</h1>
+       <h2>(<a href="/">alixetnico.com</a>)</h2>
+       <hr>
+       <p><i>Alix et Nico [...]. “Nous n'héritons pas de la terre de nos parents, nous l'empruntons à nos enfants.” #alixetnico</i></p>
+       <hr>
+       <p><a href="https://alpha.alixetnico.com/" rel="nofollow">A&N.com</a> (alpha)</p>
+       <hr>
+       <p><a href="https://beta.alixetnico.com/" rel="nofollow">A&N.com</a> (beta)</p>
+       <hr>
+       <p><a href="https://blog.alixetnico.com/">Blog</a></p>
+       <hr>
+       <p><a href="https://www.facebook.com/alixetnico/">Facebook</a> ~ <a href="https://twitter.com/alixetnico">Twitter</a> ~ <a href="https://www.instagram.com/alixetnico/">Instagram</a> ~ <a href="https://www.linkedin.com/company/alixetnico">LinkedIn</a> ~ <a href="https://medium.com/@alixetnico">Medium</a> ~ <a href="https://www.youtube.com/channel/UClpBrbAmwShlRP17ywmWj8w">YouTube</a></p>
+       <hr>
+       <p><a href="https://pdf.alixetnico.com/mentions-legales">Mentions légales</a> ~ <a href="https://img.alixetnico.com/logo/">Logo</a> ~ <a href="https://webmail.alixetnico.com/">Webmail</a></p>
+       <hr>
+       <p>/<a href="/sitemap.xml">sitemap.xml</a> ~ /<a href="/robots.txt">robots.txt</a> ~ <a href="https://www.google.com/">google.com</a> ~ <a href="https://github.com/alixetnico">GitHub</a> ~ <a href="https://developers.google.com/speed/pagespeed/insights/?hl=fr&url=https%3A%2F%2Fwww.alixetnico.com%2F">PageSpeed</a> ~ <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.alixetnico.com%2F">HTML</a> ~ <a href="https://jigsaw.w3.org/css-validator/check/referer">CSS</a> ~ <a href="https://validator.ampproject.org/#url=https%3A%2F%2Fwww.alixetnico.com%2F">AMP<amp-img src="https://img.alixetnico.com/amp.png" alt="AMP" width="12" height="12"></a></p>
+       <p>© 2019/2020 Alix & Nico ®</p>
+       <p><a href="https://gravatar.com/alixetnico"><amp-img src="https://img.alixetnico.com/logo/blanc.png" alt="A&N" width="44" height="44"></a></p>
+  </body>
+</html>
