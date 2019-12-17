f7fef56327c79513c444fc6b5aa23b0f4db1f17e
NLTdev <NLT@NLTdev.com>
2019-12-17 17:43:36 +0100

    3

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
     <meta property="og:image" content="https://img.alixetnico.com/card.png">
     <meta property="og:type" content="Organization">
     <meta property="og:url" content="https://www.alixetnico.com/">
-    <meta name="msapplication-TileImage" content="https://blog.alixetnico.com/wp-content/uploads/2019/11/cropped-b-scaled-270x270.png">
-    <link rel="icon" href="https://blog.alixetnico.com/wp-content/uploads/2019/11/cropped-b-scaled-32x32.png" sizes="32x32">
-    <link rel="icon" href="https://blog.alixetnico.com/wp-content/uploads/2019/11/cropped-b-scaled-192x192.png" sizes="192x192">
-    <link rel="apple-touch-icon-precomposed" href="https://blog.alixetnico.com/wp-content/uploads/2019/11/cropped-b-scaled-180x180.png">
+    <meta name="msapplication-TileImage" content="https://img.alixetnico.com/favicon/270.png">
+    <link rel="icon" href="https://img.alixetnico.com/favicon/32.png" sizes="32x32">
+    <link rel="icon" href="https://img.alixetnico.com/favicon/192.png" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="https://img.alixetnico.com/favicon/180.png">
     <script type="application/ld+json">
     {
     "@context" : "http://schema.org",
@@ -44,7 +44,7 @@
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
-  </head>  
+  </head>
   <body>
     <amp-analytics type="gtag" data-credentials="include">
     <script type="application/json">
@@ -63,9 +63,7 @@
        <hr>
        <p><i>Alix et Nico [...]. “Nous n'héritons pas de la terre de nos parents, nous l'empruntons à nos enfants.” #alixetnico</i></p>
        <hr>
-       <p><a href="https://alpha.alixetnico.com/" rel="nofollow">A&N.com</a> (alpha)</p>
-       <hr>
-       <p><a href="https://beta.alixetnico.com/" rel="nofollow">A&N.com</a> (beta)</p>
+       <p><a href="https://beta.alixetnico.com/" rel="nofollow">A&N.com</a> <sup>(beta)</sup></p>
        <hr>
        <p><a href="https://blog.alixetnico.com/">Blog</a></p>
        <hr>
@@ -74,7 +72,6 @@
        <p><a href="https://pdf.alixetnico.com/mentions-legales">Mentions légales</a> ~ <a href="https://img.alixetnico.com/logo/">Logo</a> ~ <a href="https://webmail.alixetnico.com/">Webmail</a></p>
        <hr>
        <p>/<a href="/sitemap.xml">sitemap.xml</a> ~ /<a href="/robots.txt">robots.txt</a> ~ <a href="https://www.google.com/">google.com</a> ~ <a href="https://github.com/alixetnico">GitHub</a> ~ <a href="https://developers.google.com/speed/pagespeed/insights/?hl=fr&url=https%3A%2F%2Fwww.alixetnico.com%2F">PageSpeed</a> ~ <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.alixetnico.com%2F">HTML</a> ~ <a href="https://jigsaw.w3.org/css-validator/check/referer">CSS</a> ~ <a href="https://validator.ampproject.org/#url=https%3A%2F%2Fwww.alixetnico.com%2F">AMP<amp-img src="https://img.alixetnico.com/amp.png" alt="AMP" width="12" height="12"></a></p>
-       <p>© 2019 Alix & Nico ®</p>
-       <p><a href="https://gravatar.com/alixetnico"><amp-img src="https://img.alixetnico.com/logo/blanc.png" alt="A&N" width="44" height="44"></a></p>
+       <p>© 2019 Alix & Nico <a href="https://gravatar.com/alixetnico"><amp-img src="https://img.alixetnico.com/logo/blanc.png" alt="A&N" width="14" height="14"></a></p>
   </body>
 </html>
